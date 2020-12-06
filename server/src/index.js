046aca63b0a5dbbfa81bbb700b1c48ade89d10df
josh4500 <jibo.ajosh45@gmail.com>
2020-12-06 18:14:29 +0100

    Create Server

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const { ApolloServer, gql } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schema");
+
+const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to DB"));
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () => console.log("Server is running at"));
