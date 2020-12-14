938edc76e2a7e056ffa593ab040b32b2ec8fa9bb
josh4500 <jibo.ajosh45@gmail.com>
2020-12-14 15:46:09 +0100

    add graphql query

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,14 +1,37 @@
 const { gql } = require("apollo-server-express");
 
+let post = [
+  { post_id: 1, post: "All is well" },
+  { post_id: 2, post: "All is well" },
+  { post_id: 3, post: "All is well" },
+];
+
 const typeDefs = gql`
   type Query {
     hello: String!
+  }
+  type User {
+    id: ID!
+    username: String!
+  }
+  input UserInfo {
+    username: String!
+    password: String!
+  }
+  type Mutation {
+    register(UserInfo: UserInfo!): User
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => "Hello world!!",
+  },
+  Mutation: {
+    register: () => ({
+      id: 12345,
+      username: "Davinci",
+    }),
   },
 };
 
